e7ccfec95bee2bf491fb7ff136b31567b1cfc9be
Latif Yildirim <113920287+latifyildirim@users.noreply.github.com>
2023-05-31 16:41:40 +0200

    Update latif.groovy

--- latif.groovy
+++ latif.groovy
@@ -19,8 +19,8 @@ job('latif-Via-DSL') {
             // properties(skipTests: true)
             mavenInstallation('maven-3.9.2')  // Bunu 'Global Tool Configuration' daki Maven Name ile ayni olmali
             // providedSettings('central-mirror')
-//             rootPOM('5-Java-tomcat-sample-main-and-Deployment-jenkins/pom.xml')  // yukaridaki 4. satir ile baglantili 
-            rootPOM('pom.xml')
+            rootPOM('5-Java-tomcat-sample-main-and-Deployment-jenkins/pom.xml')  // yukaridaki 4. satir ile baglantili 
+//             rootPOM('pom.xml')
         }
     }
 }
