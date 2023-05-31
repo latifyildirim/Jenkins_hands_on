daeb77ea0c200a0680f3adcf7421e900063026d4
latifyildirim <lyildirim.3017@gmail.com>
2023-05-31 17:47:21 +0200

    Alt klasörden yol verme islemi basarili olarak gerceklestirildi. gecmis olsun

--- latif.groovy
+++ latif.groovy
@@ -3,7 +3,7 @@ job('latif-Via-DSL') {
     scm {
         // github('https://github.com/latifyildirim/Jenkins_hands_on.git')        // Burada  5-Java-tomcat-sample-main-and-Deployment-jenkins/pom.xml   dosyam alt dizinde olmadigi icin calistiramadim, sonra bakacagim.
         // git('https://github.com/clarusway-aws-devops/java-tomcat-sample-main.git', 'main')
-        git('https://github.com/latifyildirim/Jenkins_hands_on.git', 'main')
+        git('https://github.com/latifyildirim/Jenkins_hands_on.git', 'main')    // Bu sekilde calistiii Alt Klasörlerden de yol verebiliyoruz:D:D:D:D:D
     }
     triggers {
         scm('* * * * *')
@@ -20,7 +20,7 @@ job('latif-Via-DSL') {
             // properties(skipTests: true)
             mavenInstallation('maven-3.9.2')  // Bunu 'Global Tool Configuration' daki Maven Name ile ayni olmali
             // providedSettings('central-mirror')
-            rootPOM('5-Java-tomcat-sample-main-and-Deployment-jenkins/pom.xml')  // yukaridaki 4. satir ile baglantili 
+            rootPOM('5-Java-tomcat-sample-main-and-Deployment-jenkins/pom.xml')  // yukaridaki 4. satir ile baglantili #### Ve calistiii :D:D:D:D:D
             // rootPOM('pom.xml')
         }
     }
