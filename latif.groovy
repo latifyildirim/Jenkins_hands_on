4bdeaebdb95fdb93a923bd0bb5ec26c09eabafb1
latifyildirim <lyildirim.3017@gmail.com>
2023-05-31 17:49:40 +0200

    :D

--- latif.groovy
+++ latif.groovy
@@ -29,5 +29,5 @@ job('latif-Via-DSL') {
         // archiveJunit('**/target/surefire-reports/*.xml')
     }
 }
-// Bu Web sayfasindan gerekli yardimi aldik.
+// Bu Web sayfasindan gerekli yardimi aldik. :D
 // https://jenkinsci.github.io/job-dsl-plugin/#path/job
