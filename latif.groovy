b3ccda37cea82215774d25f9eacd5ad3274eaa63
latifyildirim <lyildirim.3017@gmail.com>
2023-05-31 15:31:04 +0200

    add latif.goovy

---
+++ latif.groovy
@@ -0,0 +1,26 @@
+job('latif-Via-DSL') { 
+    description('My first job DSL')
+    scm {
+        github('https://github.com/latifyildirim/Jenkins_hands_on.git')
+    }
+    triggers {
+        scm('* * * * *')
+    }
+    steps {
+        // shell('echo START')
+        // gradle('check')
+        maven {
+            goals('clean')
+            goals('package')
+            // mavenOpts('-Xms256m')
+            // mavenOpts('-Xmx512m')
+            // localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
+            // properties(skipTests: true)
+            mavenInstallation('maven-3.9.2')  // Bunu 'Global Tool Configuration' daki Maven Name ile ayni olmali
+            // providedSettings('central-mirror')
+            rootPOM('pom.xml')
+        }
+    }
+}
+// Buradan aldik
+// https://jenkinsci.github.io/job-dsl-plugin/#path/job
